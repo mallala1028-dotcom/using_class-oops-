c63f12754fd7930f16e2339f0e592e7922bde853
mallala1028-dotcom <mallala.1028@gmail.com>
2026-06-20 18:17:08 +0530

    Update using_class.sv

--- using_class.sv
+++ using_class.sv
@@ -1,3 +1,31 @@
+class sv_class;
+  //class properties
+  int x;
+
+  //method-1
+  task set(int i);
+    x = i;
+  endtask
+
+  //method-2
+  function int get();
+    return x;
+  endfunction
+endclass
+
+module sv_class_ex;
+ sv_class class_1; //Creating Handle
+
+  initial begin
+    sv_class class_2 = new(); //Creating handle and Object
+    class_1 = new(); //Creating Object for the Handle
+    //Accessing Class methods
+    class_1.set(10);
+    class_2.set(20);
+    $display("\tclass_1 :: Value of x = %0d",class_1.get());
+    $display("\tclass_2 :: Value of x = %0d",class_2.get());
+  end
+endmodule
 
 
 
